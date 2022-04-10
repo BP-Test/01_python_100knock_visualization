521db1d0a8377459da0288c118cf595be75cdad5
BP-Test <tatsuro.0315@keio.jp>
2022-04-10 21:14:47 +0900

    Update to make all columns as 'object'

--- chapter-1/src/load_data.py
+++ chapter-1/src/load_data.py
@@ -27,14 +27,14 @@ def get_data(file_name, master=False):
     """
     if master == True:
         master_data = pd.read_csv(
-            filepath_or_buffer=f'{__dir__}/data/{str(file_name)}', delimiter='\t', encoding='shift-jis')
+            filepath_or_buffer=f'{__dir__}/data/{str(file_name)}', delimiter='\t', encoding='shift-jis', dtype='object')
         return master_data
     else:
         column_master_data = pd.read_csv(
-            filepath_or_buffer=f'{__dir__}/data/{str("mst_column_name.txt")}', delimiter='\t', encoding='shift-jis')
+            filepath_or_buffer=f'{__dir__}/data/{str("mst_column_name.txt")}', delimiter='\t', encoding='shift-jis', dtype='object')
         columns = column_master_data.column_name_en
         data = pd.read_csv(
-            filepath_or_buffer=f'{__dir__}/data/{str(file_name)}', encoding='shift-jis', header=None)
+            filepath_or_buffer=f'{__dir__}/data/{str(file_name)}', encoding='shift-jis', header=None, dtype='object')
         data.columns = columns
         return data
 
@@ -50,19 +50,20 @@ def get_diff_data(file_name, diff_file=True):
     """
     # get column names from master data
     column_master_data = pd.read_csv(
-        filepath_or_buffer=f'{__dir__}/data/{str("mst_column_name.txt")}', delimiter='\t', encoding='shift-jis')
+        filepath_or_buffer=f'{__dir__}/data/{str("mst_column_name.txt")}', delimiter='\t', encoding='shift-jis', dtype='object')
     columns = column_master_data.column_name_en
     if diff_file == True:
         # get list of data using regular expression
         data_list = glob(f'{__dir__}/data/{str(file_name)}')
         # create a base dataframe that will be appended over loops
         column_base = pd.read_csv(
-            data_list[0], encoding='shift-jis', header=None).columns.tolist()
-        data = pd.DataFrame(columns=column_base)
+            data_list[0], encoding='shift-jis', header=None, dtype='object').columns.tolist()
+        data = pd.DataFrame(columns=column_base, dtype='object')
         for i in data_list:
             data = data.append(pd.read_csv(
                 i, encoding='shift-jis', header=None))
         data.columns = columns
+        data = data.astype(str).replace('nan',np.nan)
         return data
     else:
         return column_master_data
@@ -75,13 +76,16 @@ if __name__ == '__main__':
     mst = get_data(file_name='mst_column_name.txt', master=True)
     print('Return Master data')
     display(mst.head(3))
+    display(mst.info(verbose=True))
 
     # Read all shizuoka data
     data = get_data(file_name='22_shizuoka_all_20210331.csv', master=False)
     print('Return Data after Renaming Column')
     display(data.head(3))
+    display(data.info(verbose=True))
 
     # Read all diff data
     diff_data = get_diff_data(file_name="diff*.csv", diff_file=True)
     print('Iteration Data Test')
     display(diff_data.head(3))
+    display(diff_data.info(verbose=True))
